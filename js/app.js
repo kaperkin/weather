4a11bc15f19a555a13c5cfff5e9593bc8c2e043d
kaperkin <kaleena.perkins@gmail.com>
2017-05-17 20:50:57 -0700

    Api call using zip code on button click

--- js/app.js
+++ js/app.js
@@ -1,27 +1,33 @@
 (function ($) {
-    $(document).ready(function () {
-        //if (navigator.geolocation) {
-        //    console.log('navigator.geolocation found');
-        //    navigator.geolocation.getCurrentPosition(function (position) {
-        //        console.log(position);
-        //        $('#myLocation').html("found");
-        //    }, function (err) {
-        //        $('#myLocation').html(err.code + ": " + err.message);
-        //        console.log(err.code + ": " + err.message);
-        //    })
-        //}
-        var lat = -16.92;
-        var lon = 145.77;
-        var baseURL = 'https://crossorigin.me/http://api.openweathermap.org/data/2.5/forecast/weather?lat=' + lat + '&lon=' + lon + "&APPID=b05db25a78ee7104abd3a9b1f46133b5";
-        $.getJSON(baseURL).done(function (data) {
-       //    var resData = JSON.parse(data);
-            $('#api').text("Latitude: " + data.city.coord.lat + " Longitude" +  data.city.coord.lon);
-            console.log("JSON name: " + data.name);
-            console.log(data);
-         //   console.log(resData.coord.lon);
-         //  console.log("Response Data: " + resData);
-        }).fail(function (jqxhr, textStatus, err) {
-            $('#api').html(textStatus + ": " + err);
-        });
-    })
+  $(document).ready(function () {
+    //  if (navigator.geolocation) {
+    //    console.log('navigator.geolocation found');
+    //  navigator.geolocation.getCurrentPosition(function (position) {
+    //    console.log(position);
+    //                $('#myLocation').html("found");
+    //            }, function (err) {
+    //                $('#myLocation').html(err.code + ": " + err.message);
+    //                console.log(err.code + ": " + err.message);
+    //            })
+    //        }
+    var submitBtn = $('#submit');
+    var lat = -16.92;
+    var lon = 145.77;
+    var zip = $('#zipcode').val();
+    var baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zip + "&APPID=b05db25a78ee7104abd3a9b1f46133b5";
+    submitBtn.click(function(){
+      $.getJSON(baseURL).done(function (data) {
+        var resData = JSON.stringify(data);
+        console.log(resData);
+        resData = $.parseJSON(resData);
+        var weather = resData.weather[0];
+        $('#api').text(weather["description"]);
+      }).fail(function (jqxhr, textStatus, err) {
+        $('#api').html(textStatus + ": " + err);
+      });
+    });
+
+
+//  }
+})
 }(jQuery));
