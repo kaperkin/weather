65b357cf3770e2759ad1e4e9fc50a209e485aa9d
Kaleena Perkins <kaleena.perkins@gmail.com>
2017-05-26 21:28:34 -0700

    add api key to image src

--- js/app.js
+++ js/app.js
@@ -15,7 +15,8 @@ console.log(err.code + ": " + err.message);
 // Map sample
 function initMap(lat, long) {
   $('#map').empty();
-  var src = "src='https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + long + "&amp;zoom=13&amp;size=400x500&amp;maptype=hybrid&amp;markers=color:#4256f4|" + lat + "|" + long;
+  var src = "src='https://maps.googleapis.com/maps/api/staticmap?center=" + lat
+  + "," + long + "&amp;zoom=13&amp;size=400x500&amp;maptype=hybrid&amp;key=AIzaSyB6c7H_mSZpiBv5btI2OdydpmZriPX6HWs";
         $('#map').append("<img " + src +"'/>")
       }
 ///end map sample
